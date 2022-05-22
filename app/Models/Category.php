213426d3b4babc2c75fc49bacdcf98a9a9eb1566
takeshi_honda <honda0924@hotmail.co.jp>
2022-05-22 15:58:41 +0900

    feat: create tables

---
+++ app/Models/Category.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Category extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'description',
+        'color'
+    ];
+
+    public function tasks()
+    {
+        return $this->belongsToMany('App\Model\Task')->withTimestamps();
+    }
+}
---
+++ app/Models/Task.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Task extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'content',
+        'priority',
+        'severity',
+        'status',
+        'done'
+    ];
+
+    public function tasks()
+    {
+        return $this->belongsToMany('App\Model\Category')->withTimestamps();
+    }
+}
