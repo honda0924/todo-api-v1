a26bcbe07772b5128bfea6b89e4d19947ad9baa3
takeshi_honda <honda0924@hotmail.co.jp>
2022-05-22 19:50:08 +0900

    fix: db user password

--- config/database.php
+++ config/database.php
@@ -49,7 +49,7 @@ return [
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'api-todo-v1'),
-            'username' => env('DB_USERNAME', 'sail'),
+            'username' => env('DB_USERNAME', 'root'),
             'password' => env('DB_PASSWORD', 'password'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
