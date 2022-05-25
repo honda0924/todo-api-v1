44685d95b9e4bd18d6c0c9ef93d360d143712241
takeshi_honda <honda0924@hotmail.co.jp>
2022-05-25 23:59:58 +0900

    feat: routing

--- routes/api.php
+++ routes/api.php
@@ -17,6 +17,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [TaskController::class, 'index']);
 
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
 // Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 //     return $request->user();
 // });
